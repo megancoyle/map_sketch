451aa612c3eb6bc42e43512cae708701bb219b39
Megan Coyle <meg@mcoyle.com>
2016-07-31 20:14:33 -0400

    Add new drawing form

---
+++ app/controllers/drawings_controller.rb
@@ -0,0 +1,4 @@
+class DrawingsController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :drawings
   root 'welcome#index'
 end
