3224dfd658e933e62b8088ef58cac0dcfe0c8a41
Megan Coyle <meg@mcoyle.com>
2016-07-31 21:41:31 -0400

    Add edit functionality

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -11,6 +11,10 @@ class DrawingsController < ApplicationController
     @drawing = Drawing.new
   end
 
+  def edit
+    @drawing = Drawing.find(params[:id])
+  end
+
   def create
     @drawing = Drawing.new(drawing_params)
 
@@ -18,6 +22,16 @@ class DrawingsController < ApplicationController
       redirect_to @drawing
     else
       render 'new'
+    end
+  end
+
+  def update
+    @drawing = Drawing.find(params[:id])
+
+    if @drawing.update(drawing_params)
+      redirect_to @drawing
+    else
+      render 'edit'
     end
   end
 
