5a5806bbaaff817c46baad90cfaf363fa2bc93b9
Megan Coyle <meg@mcoyle.com>
2016-08-01 09:03:31 -0400

    Add ability to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to drawing_path(@drawing)
   end
 
+def destroy
+  @drawing = Drawing.find(params[:drawing_id])
+  @comment = @drawing.comments.find(params[:id])
+  @comment.destroy
+  redirect_to drawing_path(@drawing)
+end
+
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -1,5 +1,5 @@
 class Drawing < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
