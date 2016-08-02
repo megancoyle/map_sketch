cb5d70f4647c98de39df5aeb3b47df13958d7cc2
Megan Coyle <meg@mcoyle.com>
2016-08-02 11:25:23 -0400

    Add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :couldnt_find_record
+
+  private
+  def couldnt_find_record
+    redirect_to root_url, notice: "That record doesn't exist!"
+  end
+
 end
