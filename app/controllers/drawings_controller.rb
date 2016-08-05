e6551e3a6c4b5e59c9091a2b67b6e692d90a28d2
Megan Coyle <meg@mcoyle.com>
2016-08-05 09:12:04 -0400

    Add devise

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,4 +1,5 @@
 class DrawingsController < ApplicationController
+  # before_action :authenticate_user!
   def index
     @drawings = Drawing.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :drawings do
     resources :comments
   end
