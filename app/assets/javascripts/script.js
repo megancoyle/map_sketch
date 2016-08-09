2464af977f26d7ededd8a424864d2c5a2ca2a17c
Megan Coyle <meg@mcoyle.com>
2016-08-08 21:42:48 -0400

    Add page specific js

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,68 +1,42 @@
+$(".drawings.new").ready(function() {
+  var canvas, ctx, bMouseIsDown = false, iLastX, iLastY
+      canvas = document.getElementById('cvs');
+      ctx = canvas.getContext('2d');
 
+    var img = new Image;
+    img.onload = function(){
+     ctx.drawImage(img,0,0); // Or at whatever offset you like
+    };
+    img.src = window.mapImgUrl;
 
-var canvas, ctx, bMouseIsDown = false, iLastX, iLastY,
-        $save, $imgs,
-        $convert, $imgW, $imgH,
-        $sel;
-
-    function init () {
-        canvas = document.getElementById('cvs');
-        ctx = canvas.getContext('2d');
-        $save = document.getElementById('save');
-        $convert = document.getElementById('convert');
-        $sel = document.getElementById('sel');
-        $imgs = document.getElementById('imgs');
-        $imgW = document.getElementById('imgW');
-        $imgH = document.getElementById('imgH');
-        bind();
+    canvas.onmousedown = function(e) {
+        bMouseIsDown = true;
+        iLastX = e.clientX - canvas.offsetLeft + (window.pageXOffset||document.body.scrollLeft||document.documentElement.scrollLeft);
+        iLastY = e.clientY - canvas.offsetTop + (window.pageYOffset||document.body.scrollTop||document.documentElement.scrollTop);
     }
-    function bind () {
-        var completeDrawing = []
-
-        canvas.onmousedown = function(e) {
-            bMouseIsDown = true;
-            iLastX = e.clientX - canvas.offsetLeft + (window.pageXOffset||document.body.scrollLeft||document.documentElement.scrollLeft);
-            iLastY = e.clientY - canvas.offsetTop + (window.pageYOffset||document.body.scrollTop||document.documentElement.scrollTop);
-        }
-        canvas.onmouseup = function() {
-            bMouseIsDown = false;
-            iLastX = -1;
-            iLastY = -1;
-        }
-        canvas.onmousemove = function(e) {
-            if (bMouseIsDown) {
-                var iX = e.clientX - canvas.offsetLeft + (window.pageXOffset||document.body.scrollLeft||document.documentElement.scrollLeft);
-                var iY = e.clientY - canvas.offsetTop + (window.pageYOffset||document.body.scrollTop||document.documentElement.scrollTop);
-                ctx.moveTo(iLastX, iLastY);
-                ctx.lineTo(iX, iY);
-                ctx.stroke();
-                ctx.strokeStyle = strokeColor;
-                iLastX = iX;
-                iLastY = iY;
-            }
-        };
-
-        var strokeColor = '#' + document.getElementById('color_value').value;
-        var colorPicker = document.getElementById('color_value');
-
-        document.getElementById('color_value').addEventListener('change',function(){
-                strokeColor = '#' + this.value;
-                console.log(this.value);
-                });
-
-        $save.onclick = function (e) {
-            var type = $sel.value,
-                w = $imgW.value,
-                h = $imgH.value;
-            Canvas2Image.saveAsImage(canvas, w, h, type);
-        }
-        $convert.onclick = function (e) {
-            var type = $sel.value,
-                w = $imgW.value,
-                h = $imgH.value;
-            $imgs.appendChild(Canvas2Image.convertToImage(canvas, w, h, type))
-        }
-
+    canvas.onmouseup = function() {
+        bMouseIsDown = false;
+        iLastX = -1;
+        iLastY = -1;
     }
+    canvas.onmousemove = function(e) {
+        if (bMouseIsDown) {
+            var iX = e.clientX - canvas.offsetLeft + (window.pageXOffset||document.body.scrollLeft||document.documentElement.scrollLeft);
+            var iY = e.clientY - canvas.offsetTop + (window.pageYOffset||document.body.scrollTop||document.documentElement.scrollTop);
+            ctx.moveTo(iLastX, iLastY);
+            ctx.lineTo(iX, iY);
+            ctx.stroke();
+            ctx.strokeStyle = strokeColor;
+            iLastX = iX;
+            iLastY = iY;
+        }
+    };
 
-    onload = init;
+    var strokeColor = '#' + document.getElementById('color_value').value;
+    var colorPicker = document.getElementById('color_value');
+
+    document.getElementById('color_value').addEventListener('change',function(){
+            strokeColor = '#' + this.value;
+            console.log(this.value);
+            });
+});
