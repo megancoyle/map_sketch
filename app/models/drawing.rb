2c583f3d8323e0720e766c6df6090b98a659a465
Megan Coyle <meg@mcoyle.com>
2016-08-09 17:33:00 -0400

    Adjust Google API image height

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -5,7 +5,7 @@ class Drawing < ApplicationRecord
   mount_uploader :drawing_image, DrawingImageUploader
 
   def get_map_url(address)
-    address_url = "https://maps.googleapis.com/maps/api/staticmap?center=#{address}&zoom=15&size=600x300&maptype=roadmap&key=#{ENV['google_api_key']}"
+    address_url = "https://maps.googleapis.com/maps/api/staticmap?center=#{address}&zoom=15&size=600x320&maptype=roadmap&key=#{ENV['google_api_key']}"
   end
 
 end
