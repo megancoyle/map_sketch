b8d9d499f675587f16a350228d0eeb58b4c82c95
Megan Coyle <meg@mcoyle.com>
2016-08-10 15:23:13 -0400

    Add authentication before user uses app

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,5 +1,5 @@
 class DrawingsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   def index
     @drawings = Drawing.all
   end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,5 @@
 class NotesController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def show
     @note = Note.find(params[:id])
