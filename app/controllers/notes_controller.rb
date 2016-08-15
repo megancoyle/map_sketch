36277d2940aa4bcc9b8f3f4d75a9390d121a8de6
Megan Coyle <meg@mcoyle.com>
2016-08-15 08:46:43 -0400

    Adjust authentication for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,5 @@
 class NotesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:show]
 
   def show
     @note = Note.find(params[:id])
