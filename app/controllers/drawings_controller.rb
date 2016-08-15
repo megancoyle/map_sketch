5cfdd70c6c0082d27b928a7c0c92e11dea0c7213
Megan Coyle <meg@mcoyle.com>
2016-08-15 08:44:27 -0400

    Adjust user authentication

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,5 +1,5 @@
 class DrawingsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:index, :show]
   def index
     @drawings = Drawing.all
   end
