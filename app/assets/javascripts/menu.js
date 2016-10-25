2cb21bb9132c5c0d66a8bf0d4220c3771ca59ebe
Megan Coyle <meg@mcoyle.com>
2016-10-25 13:10:52 -0400

    Add mobile menu

---
+++ app/assets/javascripts/menu.js
@@ -0,0 +1,37 @@
+/* Mobile Dropdown */
+$( document ).ready(function() {
+  var isDropdownOpen = false;
+    $(".mobile-menu").hide();
+    hideDiv();
+
+    function hideDropdown() {
+      $(".mobile-menu").css("display", "none");
+      isDropdownOpen = false;
+      $(".mobile-menu").fadeOut();
+    }
+    function showDropdown() {
+      $(".mobile-menu").css("display", "block");
+      isDropdownOpen = true;
+    }
+    function toggleDropdown(e) {
+    	e.preventDefault();
+        if (isDropdownOpen) {
+          hideDropdown();
+        } else {
+          showDropdown();
+      }
+    }
+    function hideDiv(){
+      if ($(window).width() > 610) {
+          hideDropdown();
+      }
+    }
+
+    $(".mobile-icon").click(toggleDropdown);
+    //$(".mobile-icon").click(function(event){
+     // event.preventDefault();
+    //});
+    $(window).resize(function(){
+          hideDiv();
+    });
+});
